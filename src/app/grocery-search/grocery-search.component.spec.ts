4d0ef7d8f7f603aa6b449a7d011a49f239a41a0f
Hamzeen. H <hamzeen@gmail.com>
2021-06-29 05:16:07 +0200

    Update grocery-search.component.spec.ts

--- src/app/grocery-search/grocery-search.component.spec.ts
+++ src/app/grocery-search/grocery-search.component.spec.ts
@@ -3,7 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppStateService } from '../shared/services/appstate.service';
 import { GrocerySearchComponent } from './grocery-search.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { groceries } from '../shared/data/groceries';
 
 describe('GrocerySearchComponent', () => {
 
@@ -41,12 +42,25 @@ describe('GrocerySearchComponent', () => {
 
   it('should perform a search when the search criteria is new', () => {
     // by default lastSearch is set to empty string. Hence, following is a new term.
-    const res = component.filterFruits('appl');
+    const res = component.filterFruits('apple');
     expect(res).toEqual(true);
   });
 
   it('should not perform a new search when search criteria is not new', () => {
     const res = component.filterFruits('');
     expect(res).toEqual(false);
+  });
+
+  it('should confirm entire grocery list is shown', () => {
+    const res = component.filterFruits('');
+    expect(component.getFilteredGroceries().length).toEqual(groceries.length);
+    expect(res).toEqual(false);
+  });
+
+  it('should confirm 2 results are shown for the search: \'ap\'', () => {
+    const res = component.filterFruits('ap');
+    expect(res).toEqual(true);
+    const expectedResultCount = 2;
+    expect(component.getFilteredGroceries().length).toEqual(expectedResultCount);
   });
 });
